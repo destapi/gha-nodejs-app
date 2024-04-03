1cd93996ba53298f35880d3258faab40d1cde517
Maina, Stephen <m41na@yahoo.com>
2024-04-02 23:05:19 -0500

    adding fake data for API

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
+const { faker } = require('@faker-js/faker');
 const express = require('express');
 const router = express.Router();
 
+function createRandomUser() {
+  return ({
+    _id: faker.string.uuid(),
+    avatar: faker.image.avatar(),
+    birthday: faker.date.birthdate(),
+    email: faker.internet.email(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    sex: faker.person.sexType(),
+    subscriptionTier: faker.helpers.arrayElement(['free', 'basic', 'business']),
+  });
+}
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  let users = [];
+  for (let i = 0; i < 10; i++) {
+    users.push(createRandomUser())
+  }
+  res.json(users);
 });
 
 module.exports = router;
